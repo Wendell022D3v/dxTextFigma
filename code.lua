fe80a3f2803fb4aa0565880f8054061f935cabf1
Wendell_D3v <84047883+Wendell022D3v@users.noreply.github.com>
2022-08-22 23:57:12 -0300

    Create code.lua

---
+++ code.lua
@@ -0,0 +1,3 @@
+function dxTextFigma(text, leftX, topY, rightX, bottomY, color, scaleXY, scaleY, font, alignX, alignY, clip, wordBreak, postGUI, colorCoded, subPixelPositioning, fRotation, fRotationCenterX, fRotationCenterY, fLineSpacing)
+    dxDrawText(text, leftX, topY, (leftX + rightX), (topY + bottomY), color, scaleXY, scaleY, font, alignX, alignY, clip, wordBreak, postGUI, colorCoded, subPixelPositioning, fRotation, fRotationCenterX, fRotationCenterY, fLineSpacing)    
+end
